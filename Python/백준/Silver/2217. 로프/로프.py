2471c428ea929fb5979f53a14c22ac01e76988c0
Daeun Jeong <163973110+Jeongdaeun1@users.noreply.github.com>
2025-11-10 02:28:38 +0900

    [Silver IV] Title: 로프, Time: 2424 ms, Memory: 36264 KB -BaekjoonHub

--- Python/백준/Silver/2217. 로프/로프.py
+++ Python/백준/Silver/2217. 로프/로프.py
@@ -1,15 +1,13 @@
 n = int(input())
-numli = []
+numli=[]
 for i in range(n):
-    numli.append(int(input()))#입력받은 값 리스트에 추가
-numli.sort()#오름차순 정렬
+    numli.append(int(input()))
 
-ans = 0
-maxans =0
+numli.sort()
+
+max_weight = 0
 for i in range(n):
-    ans = numli[i]*(n-i)
-    if ans >= maxans:
-        maxans = ans
+    if (numli[i])*(n-i)>= max_weight:
+        max_weight = (numli[i])*(n-i)
 
-print(maxans)
-# 3 - 10 20 30 이면 3개 다 사용시 30, 상위 두개만 사용시 40 그러면 40
+print(max_weight)
