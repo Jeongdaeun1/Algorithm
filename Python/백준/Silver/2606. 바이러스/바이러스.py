904abc00861600d826643197436499e43603e02c
Daeun Jeong <163973110+Jeongdaeun1@users.noreply.github.com>
2025-11-23 02:06:12 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

---
+++ Python/백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+N= int(input())
+M= int(input())
+# N : 정점 수, M : 간선 수
+
+cnt=[]
+
+graph = [[False]*(N+1) for i in range(N+1)]
+#전부다 False 상태인 행렬
+
+for i in range(M):
+    x, y = map(int, input().split())
+    graph[x][y] = 1
+    graph[y][x] = 1
+#인접 행렬 생성 완료
+
+visited = [False]*(N+1)
+#방문 여부 담기
+
+#DFS
+def DFS(v):
+    visited[v]=True #첫번째 노드는 무조건 방문하고 시작하기 때문에 방문 처리
+    #print(v, end=" ") #방문한 즉시 출력
+    cnt.append(v)
+    for i in range(1, N+1):
+        if not visited[i] and graph[v][i]==1:
+            DFS(i)
+
+
+
+#BFS
+def BFS(v):
+    cnt=0
+    visited = [False]*(N+1)
+    q = deque([v])
+    visited[v] = True
+
+    while q: #queue안에 데이터 없을 때 까지 실행
+        v = q.popleft()
+        #print(v, end=" ")
+        cnt.append(v)
+        for i in range(1, N+1):
+            if not visited[i] and graph[v][i] == 1:
+                q.append(i)
+                visited[i] = True
+
+DFS(1)
+print(len(cnt)-1)
